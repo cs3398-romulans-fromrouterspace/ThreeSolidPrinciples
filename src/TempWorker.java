c6d83550b92b49ecbd025d5bde10150bfba62776
Kendra Maggiore <kam355@txstate.edu>
2019-02-17 23:30:24 -0600

    Added TempWorker and Updated Main

---
+++ src/TempWorker.java
@@ -0,0 +1,27 @@
+package threesolid;
+
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+import java.io.*;
+
+import threesolid.IEat;
+import threesolid.ISick;
+
+class TempWorker extends BaseWorker implements IEat, ISick{
+
+  @Override
+  public void work() {
+		System.out.println("I do diffrent work");
+	}
+  public void eat() {
+      System.out.println("I am a temp worker, I don't eat much");
+  }
+  public void sick() {
+      System.out.println("I can't call off sick, I have no sick days.");
+  }
+
+}
+
+// Single Responsibility is in play here because this class only handels the TempWorker class and nothing else
+// Interface Segregation was used to keep the eat/sick interface  separate from the robot interface because people don't reboot
+//Open/Close was broken to have more interfaces 
--- src/ThreeSoildMain.java
+++ src/ThreeSoildMain.java
@@ -4,37 +4,60 @@ import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.*;
 
-import threesolid.Workable;
-import threesolid.Feedable;
+public class ThreeSoildMain {
 
-public class ThreeSoildMain
-{
+    public static Manager tsManager = new Manager();
 
-   public static Manager tsManager = new Manager();
+    // The entry main() method
+    public static void main(String[] args) {
+        try {
+            System.out.format("Starting ... \n"); // Start of Program
+        } catch (Exception main_except) {
+            main_except.printStackTrace();
+        }
 
-   // The entry main() method
-   public static void main(String[] args)
-   {
+        BaseWorker Kendra = new BaseWorker(); // Calls BaseWorker class
+        System.out.println("\nBaseWorker: ");
+        Kendra.work();
 
-      try
-      {
-         System.out.format("Starting ... \n");
-      }
-      catch (Exception main_except)
-      {
-         main_except.printStackTrace();
-      }
+        Worker David = new Worker(); // Calls worker class
+        System.out.println("Worker: ");
+        David.work();
+        David.eat();
+        David.sick();
 
-            try
-      {
-         System.out.format("Stopping ... \n");
-      }
-      catch (Exception main_except)
-      {
-         main_except.printStackTrace();
-      }
+        SuperWorker SuperMan = new SuperWorker(); // Calls SuperWorker Class
+        System.out.println("SuperWorker: ");
+        SuperMan.work();
+        SuperMan.eat();
+        SuperMan.sick();
 
-      System.exit(0);
+        TempWorker Joker = new TempWorker(); // Calls TempWorker Class
+        System.out.println("TempWorker: ");
+        Joker.work();
+        Joker.eat();
+        Joker.sick();
 
-   }
- }
+        Robot Watson = new Robot(); // Calls Robot Class
+        System.out.println("Robot: ");
+        Watson.work();
+        Watson.reboot();
+
+        ProductManager BatMan = new ProductManager(); // Calls ProductManager Class
+        System.out.println("Product Manager: ");
+        BatMan.defineProduct();
+
+        ProjectManager WonderWoman= new ProjectManager(); // Calls ProjectManager Class
+        System.out.println("Project Manager: ");
+        WonderWoman.scheduleWork();
+
+
+        try {
+            System.out.format("\nStopping ... \n"); //Program Stops
+        } catch (Exception main_except) {
+            main_except.printStackTrace();
+        }
+
+        System.exit(0);
+    }
+}
