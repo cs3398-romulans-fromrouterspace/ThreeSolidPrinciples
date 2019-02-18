58d35f2d480bc32dfdc45ceec51ab9dc56d59697
Hollie Wilson <holliebrooke97@yahoo.com>
2019-02-18 16:01:48 -0600

    A3 Push

---
+++ src/FeedableInterface.java
@@ -0,0 +1,14 @@
+package threesolid;
+
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+import java.io.*;
+
+/* 
+Follows Interface Segregation Principle
+Interfaces are client specific rather than general purpose
+*/
+interface Feedable {
+	public void eat();
+	
+}
