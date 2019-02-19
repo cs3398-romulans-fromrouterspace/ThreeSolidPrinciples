ec795f80173dafd7b4bb882d557fcf5d87bc4e40
Kendra Maggiore <kam355@txstate.edu>
2019-02-18 19:14:42 -0600

    Pushed A3 files

---
+++ A3/src/WorkableInterface.java
@@ -0,0 +1,10 @@
+package threesolid;
+
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+import java.io.*;
+
+interface Workable {
+	public void work();
+	
+}
