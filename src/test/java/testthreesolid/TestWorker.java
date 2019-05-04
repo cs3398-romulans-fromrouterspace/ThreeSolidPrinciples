6815ab32c0cb45e54b6baaadd498dcdd57b67ab4
Kendra Maggiore <kam355@txstate.edu>
2019-05-03 21:47:25 -0500

    Testing

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -3,10 +3,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class TestWorker {
-	BaseWorker w = new BaseWorker();
+	Worker w = new Worker();
 	@Test
 	@DisplayName("Work Method Test")
-	public void newtestKAMWorkerPass(){
-	      assertEquals(w.Work(), true);
+	public void newTestKAMWWorkerPass() {
+		assertEquals("Worker working...", w.work());
 	}
 }
