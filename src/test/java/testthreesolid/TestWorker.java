b4a28f84afdb86063893acb26d88ecde20f62330
Kendra Maggiore <kam355@txstate.edu>
2019-05-03 21:16:29 -0500

    Test

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -3,10 +3,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class TestWorker {
-	private  Worker w = new Worker();
+	BaseWorker w = new BaseWorker();
 	@Test
 	@DisplayName("Work Method Test")
-	public void testWorkMethod(){
-		assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
+	public void newtestKAMWorkerPass(){
+	      assertEquals(w.Work(), true);
 	}
 }
