e86e7f2b9c373d93d5dae0b96d43ddef0a999ca0
Kendra Maggiore <kam355@txstate.edu>
2019-05-03 22:17:33 -0500

    Update TestWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 public class TestWorker {
 	Robot w = new Robot();
 	@Test
-	@DisplayName("Work Method Test")
+	@DisplayName("Work Method Test Pass")
 //public void newTestKAMWWorkerPass() {
-//							assertEquals("Need to reboot segfault",w.reboot());
-//	}
-
+//       assertEquals("Need to reboot segfault",w.reboot());
+//}
+	@Test
+	@DisplayName("Work Method Test Fail")
 	public void newTestKAMWWorkerFail() {
-							assertEquals("Robot will not reboot",w.reboot());
+		assertEquals("Robot will not reboot",w.reboot());
 	}
 }
