adea2fd92c3a0727cf9ea9a724d4e643b921bd95
w-h37 <47009048+w-h37@users.noreply.github.com>
2019-05-04 23:27:16 -0500

    Update TestWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -6,7 +6,7 @@ public class TestWorker {
 	private  Worker w = new Worker();
 	@Test
 	@DisplayName("Work Method Test")
-	public void testWorkMethod(){
-		assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
+	public void newTestWorkMethod(){
+		assertEquals(w.work(),"This is meant to fail",”Mismatch between test text and method text”);
 	}
 }
